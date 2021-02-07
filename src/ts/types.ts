15ea14d8d85903820ec8ddf975f249636cac366c
Luiz Cristino <luizptcristino@gmail.com>
2021-02-07 15:08:50 -0300

    Create ts types

---
+++ src/ts/types.ts
@@ -0,0 +1,3 @@
+export type Coordinates = { lat: number; lng: number };
+export type SocialNetworks = { instagram: ''; website: ''; facebook: ''; twitter: ''; };
+export type PlacePrices = { perPerson: { default: number; withBeverages: number } };
