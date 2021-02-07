c8be91a988b2c4af5b78e3903e1567cf55edbf02
Luiz Cristino <luizptcristino@gmail.com>
2021-02-07 13:05:18 -0300

    Fix dev env

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
   extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
   parser: '@typescript-eslint/parser',
@@ -15,12 +16,16 @@ module.exports = {
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     '@typescript-eslint/no-use-before-define': 2,
+    'import/prefer-default-export': 0,
+    'import/no-unresolved': 0, // Disabled because is typescript who guarantees files import resolution
     'no-plusplus': [2, { allowForLoopAfterthoughts: true }],
+    'no-underscore-dangle': 0,
     'no-use-before-define': 0,
     'prettier/prettier': 2,
     'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
   },
   globals: {
     fetch: false,
+    __DEV__: 'readonly',
   },
 };
