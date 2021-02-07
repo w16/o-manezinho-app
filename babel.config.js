903f2d424e671506308df6c4bdc4a7a55eb6b236
Luiz Cristino <luizptcristino@gmail.com>
2021-02-07 13:05:56 -0300

    Configure folders structure

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@store': './src/store',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
