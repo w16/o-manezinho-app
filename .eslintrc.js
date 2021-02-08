091e9160e4e1ad338be9183d83e07e3f5bafe3ab
Luiz Cristino <luizptcristino@gmail.com>
2021-02-07 22:21:55 -0300

    Add typescript plugin to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,13 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+    'plugin:@typescript-eslint/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -16,12 +22,14 @@ module.exports = {
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     '@typescript-eslint/no-use-before-define': 2,
+    '@typescript-eslint/no-unused-vars': 2,
     'import/prefer-default-export': 0,
     'import/no-unresolved': 0, // Disabled because is typescript who guarantees files import resolution
     'import/extensions': [2, 'never', { svg: 'always' }],
     'no-plusplus': [2, { allowForLoopAfterthoughts: true }],
     'no-underscore-dangle': 0,
     'no-use-before-define': 0,
+    'no-unused-vars': 0,
     'prettier/prettier': 2,
     'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
   },
